2093745b2bd1310c2aeaa714814ce904a2a5139c
Kate Humphrey <kate@fullstackacademy.com>
2017-07-24 16:26:04 -0400

    changed update to mirror bones master

---
+++ app/main.jsx
@@ -0,0 +1,46 @@
+'use strict'
+
+/**
+ * `babel-preset-env` converts this general import into a selection of specific
+ * imports needed to polyfill the currently-supported environment (as specified
+ * in `.babelrc`). As of 2017-06-04, this is primarily to support async/await.
+ */
+import 'babel-polyfill'
+
+import React from 'react'
+import {render} from 'react-dom'
+import {Provider, connect} from 'react-redux'
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom'
+
+import store from './store'
+import Jokes from './components/Jokes'
+import Login from './components/Login'
+import WhoAmI from './components/WhoAmI'
+import NotFound from './components/NotFound'
+
+const ExampleApp = connect(
+  ({ auth }) => ({ user: auth })
+)(
+  ({ user, children }) =>
+    <div>
+      <nav>
+        {user ? <WhoAmI/> : <Login/>}
+      </nav>
+      <main>
+        <Switch from="">
+          <Route path="/jokes" component={Jokes} />
+          <Redirect exact from="/" to="/jokes" />
+          <Route component={NotFound} />
+        </Switch>
+      </main>
+    </div>
+)
+
+render(
+  <Provider store={store}>
+    <Router>
+      <ExampleApp />
+    </Router>
+  </Provider>,
+  document.getElementById('main')
+)
