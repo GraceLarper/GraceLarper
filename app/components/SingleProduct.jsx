16ccd36d886fbd6079247e7f4d970707119c9e53
Jack Jiang <thejackjiang@gmail.com>
2017-07-29 16:20:28 -0400

    DYNAMIC REVIEWS

--- app/components/SingleProduct.jsx
+++ app/components/SingleProduct.jsx
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {getSingleProduct} from '../reducers/products'
-import {getReviewsForSingleProd} from '../reducers/reviews'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { getSingleProduct } from '../reducers/products'
+import { getReviewsForSingleProd, addNewReview} from '../reducers/reviews'
 
 import Thumbnail from 'react-bootstrap/lib/Thumbnail'
 import Button from 'react-bootstrap/lib/Button'
@@ -15,89 +15,101 @@ import Well from 'react-bootstrap/lib/Well'
 
 
 
-class SingleProduct extends Component{
-  constructor(props){
+class SingleProduct extends Component {
+  constructor(props) {
     super(props);
 
-    this.handleChange = this.handleChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
   }
 
 
-  handleChange(e) {
-    this.setState({ value: e.target.value });
+  onSubmit(event) {
+    console.log(this.props)
+    const addNewReview = this.props.addNewReview;
+    event.preventDefault();
+    const newReview = {
+      comment: event.target.comment.value,
+      product_id: this.props.match.params.id,
+      user_id: this.props.auth.id
+    }
+    addNewReview(newReview)
+    event.target.comment.value = '';
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.getReviewsForSingleProd(this.props.match.params.id)
     this.props.getSingleProduct(this.props.match.params.id)
 
   }
-// {'/images/' + singleProduct.imageUrl}
-  render(){
+  // {'/images/' + singleProduct.imageUrl}
+  render() {
+    console.log(this.props)
     const singleProduct = this.props.products;
-    console.log('props:', this.props)
-    return singleProduct? (
+    return singleProduct ? (
       <div>
-      <div className="container">
-        <div className="col-xs-3">
-          <Thumbnail style={{width:256, height:256}} src={'/images/' + singleProduct.imageUrl} alt="242x200">
-          </Thumbnail>
-        </div>
-        <div className="col-xs-9">
-          <h3>{singleProduct.title}</h3>
-          <h4>{'$' + singleProduct.price}</h4>
-          <p>{singleProduct.description}</p>
-          <h5>{'Stock:' + singleProduct.quantity}</h5>
-          <p>
-            <Button bsStyle="primary">Add to Cart</Button>&nbsp;
+        <div className="container">
+          <div className="col-xs-3">
+            <Thumbnail style={{ width: 256, height: 256 }} src={'/images/' + singleProduct.imageUrl} alt="242x200">
+            </Thumbnail>
+          </div>
+          <div className="col-xs-9">
+            <h3>{singleProduct.title}</h3>
+            <h4>{'$' + singleProduct.price}</h4>
+            <p>{singleProduct.description}</p>
+            <h5>{'Stock:' + singleProduct.quantity}</h5>
+            <p>
+              <Button bsStyle="primary">Add to Cart</Button>&nbsp;
           </p>
 
-         </div>
-         </div>
-          <div className="container">
-            <h4>Reviews</h4>
-            <div className="col-xs-6">
-              {this.props.review.map(review=>{
-            return(
-              <div>
-              <img style={{height:25, width: 100}}src='/images/5star.jpg'/>
-            <Well>{review.comment}</Well>
-            </div>)})}
-            </div>
-            </div>
-         <div className= "container">
-         <div className="col-xs-8">
-         <form>
-        <FormGroup
-          controlId="formControlsTextarea">
-          <ControlLabel>Write a review</ControlLabel>
-            <FormGroup controlId="formControlsTextarea">
-          <FormControl componentClass="textarea" placeholder="Review me"
-            onChange={this.handleChange}/>
+          </div>
+        </div>
+        <div className="container">
+          <h4>Reviews</h4>
+          <div className="col-xs-6">
+            {this.props.review.map(review => {
+              return (
+                <div key={review.id}>
+                  <img style={{ height: 25, width: 100 }} src='/images/5star.jpg' />
+                  <Well>{review.comment}</Well>
+                </div>)
+            })}
+          </div>
+        </div>
+        <div className="container">
+          <div className="col-xs-8">
+            <div>
+            {this.renderNewReview()}
+          </div>
+          </div>
+        </div>
+      </div>
+    ) :
+      (
+        <div>LOADING</div>
+      )
+  }
+
+  renderNewReview() {
+    return (<form onSubmit={this.onSubmit}>
+      <FormGroup controlId="formControlsTextarea">
+        <ControlLabel>Write a review</ControlLabel>
+        <FormGroup controlId="formControlsTextarea">
+          <FormControl componentClass="textarea" name="comment" placeholder="Review me"/>
           <HelpBlock>Give us your thoughts on this product</HelpBlock>
         </FormGroup>
-        </FormGroup>
-        <Button bsStyle="primary">Submit</Button>&nbsp;
+      </FormGroup>
+      <Button bsStyle="primary" type="submit">Submit</Button>&nbsp;
       </form>
-      </div>
-      </div>
-        </div>
-    ) :
-    (
-      <div>LOADING</div>
-    )
-
-  }
+    )}
 }
 function mapStateToProps(state, componentProps) {
-  console.log('state', state)
-  console.log('componentProps', componentProps)
   return {
     products: state.products[0],
-    review: state.reviews
+    review: state.reviews,
+    auth: state.auth
   }
 }
 
 
 
-export default connect(mapStateToProps, {getSingleProduct, getReviewsForSingleProd})(SingleProduct)
+export default connect(mapStateToProps, { getSingleProduct, getReviewsForSingleProd, addNewReview })(SingleProduct)
