85da60c4a0d49210f073df176fa2a40d65a59d31
Alexander Zoitos <azoitos1@gmail.com>
2017-07-29 14:08:13 -0400

    Review routes written

--- server/api.js
+++ server/api.js
@@ -8,6 +8,7 @@ api
   .use('/users', require('./users'))
   .use('/products', require('./products'))
   .use('/orders', require('./orders'))
+  .use('/reviews', require('./reviews'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
