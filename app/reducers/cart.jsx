0ca04e34f7abd8669ac2a23a9a86aa0584e04561
Danielboufford <daniel.boufford@gmail.com>
2017-07-31 20:02:22 -0400

    updates

--- app/reducers/cart.jsx
+++ app/reducers/cart.jsx
@@ -1,26 +1,52 @@
 import axios from 'axios'
 
-const ADD_TO_CART= 'ADD_TO_CART'
+const ADD_TO_CART = 'ADD_TO_CART'
+const GET_ORDER = 'GET_ORDER'
 
-export const addToCart = product => {
+export const addToCart = (product, order) => {
   return {
     type: ADD_TO_CART,
-    product
+    product,
+    order
   }
 }
 
-const reducer = (state = [], action) => {
+export const getOrder = order => ({
+  type: GET_ORDER,
+  order
+})
+
+const reducer = (state = { products: [], order: null, productsForOrder: [] }, action) => {
   switch (action.type) {
-  case ADD_TO_CART:
-    return [...state, action.product]
-  default:
-    return state
+    case ADD_TO_CART:
+      return Object.assign({}, state, { products: [...state.products, action.product] }, { order: action.order })
+    case GET_ORDER:
+      return Object.assign({}, state, { productsForOrder: [...state.productsForOrder, action.order] })
+    default:
+      return state
   }
 }
 
-export const addToCartThunk = (product) => dispatch => {
-  axios.post('/api/orders', product)
-    .then(order => order.data)
-    .then(dispatch(addToCart(product)))
+export function addToCartThunk(product) {
+  return dispatch =>
+    axios.post(`/api/orders`, product)
+      .then(result => {
+        dispatch(addToCart(product, result.data.id))
+      })
+      .catch((e) => console.error(e))
 }
+
+export function getOrderThunk(orderId) {
+  return dispatch =>
+  axios.get(`/api/orderproducts/${orderId}`)
+  .then(order =>
+    dispatch(getOrder(order)))
+}
+
+// export const addToCartThunk = (product) => dispatch => {
+//   axios.post('/api/orders', product)
+//   .then(order => order.data)
+//   .then(dispatch(addToCart(product)))
+//   }
+
 export default reducer
